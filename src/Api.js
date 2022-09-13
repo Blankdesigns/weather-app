1b0f75a2fec8f32395c16563567a96cc0ceba558
Blankdesigns <ebun.sonuyi@gmail.com>
2022-09-13 14:24:15 +0100

    weather-app

---
+++ src/Api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '70325955fbmsh74c589bd9687acbp1a7d4cjsn383dbfaca6e1',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "6477a9e662bddd4b3a5624891430880b";
